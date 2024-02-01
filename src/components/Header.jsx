b642729431c5a43f4c2da39c753e3a7ae07062c2
sergio hernandez <sergiorhv@gmail.com>
2024-01-31 23:27:26 -0400

    actiualizacion en github

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (  
-        <h1 className="font-black text-5xl text-center md:w-1/2 mx-auto ">Seguimiento Pacientes de {""}
+        <h1 className="font-black text-5xl text-center md:w-1/2 mx-auto ">Seguimiento Pacientes de {""} Con GitHub {""}
         <spam className="font-black text-5xl text-indigo-600 ">veterinaria</spam></h1>
     );
 }
